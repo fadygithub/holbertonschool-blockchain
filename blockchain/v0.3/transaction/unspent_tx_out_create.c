83b95f7ea423b8f9bc07fd3fa5c4e94d9f82a5f8
FADY HANNA <fadyhanna@FADYs-MacBook-Pro.local>
2022-01-14 23:41:14 +0200

    debug blockchain v0.3

--- blockchain/v0.3/transaction/unspent_tx_out_create.c
+++ blockchain/v0.3/transaction/unspent_tx_out_create.c
@@ -8,17 +8,17 @@
 * Return: an unspent transaction output
 */
 unspent_tx_out_t *unspent_tx_out_create(uint8_t
-                                        block_hash[SHA256_DIGEST_LENGTH],
-                                        uint8_t tx_id[SHA256_DIGEST_LENGTH],
-                                        tx_out_t const *out)
+										block_hash[SHA256_DIGEST_LENGTH],
+										uint8_t tx_id[SHA256_DIGEST_LENGTH],
+										tx_out_t const *out)
 {
-    unspent_tx_out_t *tr;
+	unspent_tx_out_t *tr;
 
-    tr = malloc(sizeof(*tr));
-    if (!tr)
-        return (NULL);
-    memcpy(tr->block_hash, block_hash, SHA256_DIGEST_LENGTH);
-    memcpy(tr->tx_id, tx_id, SHA256_DIGEST_LENGTH);
-    memcpy(&tr->out, out, sizeof(*out));
-    return (tr);
+	tr = malloc(sizeof(*tr));
+	if (!tr)
+		return (NULL);
+	memcpy(tr->block_hash, block_hash, SHA256_DIGEST_LENGTH);
+	memcpy(tr->tx_id, tx_id, SHA256_DIGEST_LENGTH);
+	memcpy(&tr->out, out, sizeof(*out));
+	return (tr);
 }
