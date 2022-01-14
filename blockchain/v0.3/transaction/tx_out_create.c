1b5d698f8fff3ba702b4632b543f17299326417e
FADY HANNA <fadyhanna@FADYs-MacBook-Pro.local>
2022-01-14 23:36:11 +0200

    debug blockchain v0.3

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -1,4 +1,5 @@
 #include "transaction.h"
+
 /**
 * tx_out_create - creates a transaction output
 * @amount: amount of the transaction
@@ -7,19 +8,19 @@
 */
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 {
-    tx_out_t *tr;
+	tx_out_t *tr;
 
-    if (!pub)
-        return (NULL);
-    tr = calloc(1, sizeof(*tr));
-    if (!tr)
-        return (NULL);
-    tr->amount = amount;
-    memcpy(tr->pub, pub, EC_PUB_LEN);
-    if (!sha256((const int8_t *)tr, TX_OUT_HASH_LEN, tr->hash))
-    {
-        free(tr);
-        return (NULL);
-    }
-    return (tr);
+	if (!pub)
+		return (NULL);
+	tr = calloc(1, sizeof(*tr));
+	if (!tr)
+		return (NULL);
+	tr->amount = amount;
+	memcpy(tr->pub, pub, EC_PUB_LEN);
+	if (!sha256((const int8_t *)tr, TX_OUT_HASH_LEN, tr->hash))
+	{
+		free(tr);
+		return (NULL);
+	}
+	return (tr);
 }
