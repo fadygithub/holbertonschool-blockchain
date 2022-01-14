76e05f2cf4b8c33c373f1264ba5684b1450498ef
FADY HANNA <fadyhanna@FADYs-MacBook-Pro.local>
2022-01-14 23:47:04 +0200

    debug blockchain v0.3

--- blockchain/v0.3/transaction/transaction_hash.c
+++ blockchain/v0.3/transaction/transaction_hash.c
@@ -1,4 +1,3 @@
-
 #include "transaction.h"
 
 /**
@@ -10,8 +9,8 @@
 */
 int tin_to_buffer(llist_node_t tr, unsigned int idx, void *buffer)
 {
-    memcpy((int8_t *)buffer + idx * TX_IN_HASH_VAL_LEN, tr, TX_IN_HASH_VAL_LEN);
-    return (0);
+	memcpy((int8_t *)buffer + idx * TX_IN_HASH_VAL_LEN, tr, TX_IN_HASH_VAL_LEN);
+	return (0);
 }
 
 /**
@@ -23,10 +22,10 @@ int tin_to_buffer(llist_node_t tr, unsigned int idx, void *buffer)
 */
 int tout_to_buffer(llist_node_t tr, unsigned int idx, void *buffer)
 {
-    memcpy((int8_t *)buffer + idx * TX_OUT_HASH_VAL_LEN,
-           ((tx_out_t *)tr)->hash,
-           TX_OUT_HASH_VAL_LEN);
-    return (0);
+	memcpy((int8_t *)buffer + idx * TX_OUT_HASH_VAL_LEN,
+		   ((tx_out_t *)tr)->hash,
+		   TX_OUT_HASH_VAL_LEN);
+	return (0);
 }
 
 /**
@@ -36,26 +35,27 @@ int tout_to_buffer(llist_node_t tr, unsigned int idx, void *buffer)
 * Return: @hash_buf if success, NULL on failure
 */
 uint8_t *transaction_hash(transaction_t const *transaction,
-                          uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+						  uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-    size_t tin_size, tout_size;
-    int8_t *buffer;
+	size_t tin_size, tout_size;
+	int8_t *buffer;
 
-    if (!transaction || !hash_buf || llist_size(transaction->inputs) == -1
-        || llist_size(transaction->outputs) == -1)
-    {
-        return (NULL);
-    }
-    tin_size = (size_t)llist_size(transaction->inputs) * TX_IN_HASH_VAL_LEN;
-    tout_size = (size_t)llist_size(transaction->outputs) * TX_OUT_HASH_VAL_LEN;
-    buffer = calloc(1, tin_size + tout_size);
-    if (!buffer)
-    {
-        return (NULL);
-    }
-    llist_for_each(transaction->inputs, tin_to_buffer, buffer);
-    llist_for_each(transaction->outputs, tout_to_buffer, buffer + tin_size);
-    sha256(buffer, tin_size + tout_size, hash_buf);
-    free(buffer);
-    return (hash_buf);
+	if (!transaction || !hash_buf || llist_size(transaction->inputs) == -1
+		|| llist_size(transaction->outputs) == -1)
+	{
+		return (NULL);
+	}
+	tin_size = (size_t)llist_size(transaction->inputs) * TX_IN_HASH_VAL_LEN;
+	tout_size = (size_t)llist_size(transaction->outputs) * TX_OUT_HASH_VAL_LEN;
+	buffer = calloc(1, tin_size + tout_size);
+	if (!buffer)
+	{
+		return (NULL);
+	}
+	llist_for_each(transaction->inputs, tin_to_buffer, buffer);
+	llist_for_each(transaction->outputs, tout_to_buffer, buffer + tin_size);
+	sha256(buffer, tin_size + tout_size, hash_buf);
+	free(buffer);
+	return (hash_buf);
 }
+
