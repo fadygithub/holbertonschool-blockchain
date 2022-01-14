e7395e5a1b099e3065ed8ab55301087d11d5fb31
FADY HANNA <fadyhanna@FADYs-MacBook-Pro.local>
2022-01-15 00:25:09 +0200

    debug blockchain v0.3

--- blockchain/v0.3/transaction/update_unspent.c
+++ blockchain/v0.3/transaction/update_unspent.c
@@ -1,104 +1,55 @@
 #include "transaction.h"
-
-
-/**
-* find_unspent - finds unspent matching txo
-* @node: current node, unspent_txo_t
-* @arg: txo id
-* Return: 0 if continue else 1
-*/
-int find_unspent(llist_node_t node, void *arg)
-{
-unspent_tx_out_t *utxo = node;
-
-if (!memcmp(arg, utxo->out.hash, SHA256_DIGEST_LENGTH))
-return (1);
-return (0);
-}
+#include <string.h>
 
 /**
-* foreach_input - maps output to input txs
-* @node: current node, txi
-* @idx: index of node
-* @_args: arguments
-* Return: 0 if continue else 1
-*/
-int foreach_input(llist_node_t node, unsigned int idx, void *_args)
+ * find_unspent - finds an unspent transaction output from a list of
+ *                unspent transaction outputs
+ * @node: an unspent transaction output
+ * @hash: hash of transaction output to remove
+ * Return: 1 on successful removal, 0 if hash not found or error occured
+ **/
+int find_unspent(llist_node_t node, void *hash)
 {
-void **args = _args;
-tx_in_t *txi = node;
+	unspent_tx_out_t *unspent = (unspent_tx_out_t *)node;
 
-llist_remove_node(args[0], find_unspent, txi->tx_out_hash, 1, free);
-return (0);
-(void)idx;
-(void)txi;
-(void)args;
+	return (!memcmp(unspent->out.hash, hash, sizeof(unspent->out.hash)));
 }
 
 /**
-* foreach_output - maps output to input txs
-* @node: current node, txo
-* @idx: index of node
-* @_args: arguments
-* Return: 0 if continue else 1
-*/
-int foreach_output(llist_node_t node, unsigned int idx, void *_args)
+ * update_unspent - updates the list of unspent transaction outputs, given a
+ *                  list of processed transactions
+ * @transactions: list of validated transactions
+ * @block_hash: hash of the validated block that contains `transactions`
+ * @all_unspent: list of unspent transaction outputs.
+ * Return: updated list of unspent transaction outputs
+ **/
+llist_t *update_unspent(llist_t *transactions, uint8_t block_hash[SHA256_DIGEST_LENGTH], llist_t *all_unspent)
 {
-void **args = _args;
-tx_out_t *txo = node;
-unspent_tx_out_t *utxo;
+	int i, j, tmp, size;
+	transaction_t *tx;
+	tx_in_t *in;
+	tx_out_t *out;
+	unspent_tx_out_t *unspent;
 
-utxo = unspent_tx_out_create(args[1], args[2], txo);
-if (!utxo)
-{
-dprintf(2, "foreach_output: failed to create UTXO.\n");
-exit(1);
-}
+	if (!transactions || !block_hash || !all_unspent)
+		return (all_unspent);
 
-if (llist_add_node(args[0], utxo, ADD_NODE_REAR))
-{
-dprintf(2, "foreach_output: failed to add node.\n");
-exit(1);
-}
-return (0);
-(void)idx;
-}
+	for (i = 0, size = llist_size(transactions); i < size; i++)
+	{
+		tx = llist_get_node_at(transactions, i);
+		for (j = 0, tmp = llist_size(tx->inputs); j < tmp; j++)
+		{
+			in = llist_get_node_at(tx->inputs, j);
+			llist_remove_node(all_unspent, find_unspent, in->tx_out_hash, 1, NULL);
+		}
+		for (j = 0, tmp = llist_size(tx->outputs); j < tmp; j++)
+		{
+			out = llist_get_node_at(tx->outputs, j);
+			unspent = unspent_tx_out_create(block_hash, tx->id, out);
+			llist_add_node(all_unspent, unspent, ADD_NODE_REAR);
+		}
+	}
 
-/**
-* foreach_transaction - maps output to input txs
-* @node: current node, tx
-* @idx: index of node
-* @__args: arguments
-* Return: 0 if continue else 1
-*/
-int foreach_transaction(llist_node_t node, unsigned int idx, void *__args)
-{
-transaction_t *tx = node;
-void *args[3] = {0}, **_args = __args;
-
-args[0] = _args[0], args[1] = _args[1], args[2] = tx->id;
-llist_for_each(tx->inputs, foreach_input, args);
-llist_for_each(tx->outputs, foreach_output, args);
-return (0);
-(void)idx;
-}
-/**
-* update_unspent - updates list of UTXOs
-* @transactions: list of validate txs
-* @block_hash: hash of block containing txs
-* @all_unspent: list of all UTXOs
-* Return: new UTXO list
-*/
-llist_t *update_unspent(llist_t *transactions,
-			uint8_t block_hash[SHA256_DIGEST_LENGTH], llist_t *all_unspent)
-{
-void *args[2] = {0};
-
-args[0] = all_unspent, args[1] = block_hash;
-llist_for_each(transactions, foreach_transaction, args);
-(void)transactions;
-(void)block_hash;
-(void)all_unspent;
-return (all_unspent);
+	return (all_unspent);
 }
 
